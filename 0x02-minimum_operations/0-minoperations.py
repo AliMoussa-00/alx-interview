71a434ca4d725afbf1042debdcf95456eff81fee
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-04-23 20:51:32 +0100

    trying DP

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,28 +4,43 @@
 import math
 
 
+# def minOperations(n: int) -> int:
+#     '''
+#     Calculates the sum of prime factors of a given positive integer "n"
+#     Time Complexity:  O(sqrt(n))
+#     '''
+#     if n <= 0 or not isinstance(n, int):
+#         return 0
+
+#     total = 0
+#     factor = 2
+#     sqrt_n = int(math.sqrt(n))
+
+#     while factor <= sqrt_n:
+#         if n <= 1:
+#             break
+
+#         if n % factor == 0:
+#             total += factor
+
+#             n //= factor
+#             continue
+
+#         factor += 1
+
+#     return total
 def minOperations(n: int) -> int:
     '''
     Calculates the sum of prime factors of a given positive integer "n"
     Time Complexity:  O(sqrt(n))
     '''
-    if n <= 0 or not isinstance(n, int):
+    if n <= 1:
         return 0
 
-    total = 0
-    factor = 2
-    sqrt_n = int(math.sqrt(n))
-
-    while factor <= sqrt_n:
-        if n <= 1:
-            break
-
-        if n % factor == 0:
-            total += factor
-
-            n //= factor
-            continue
-
-        factor += 1
-
-    return total
+    dp = [0] * (n + 1)
+    for i in range(2, n + 1):
+        dp[i] = float('inf')
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+    return int(dp[n])
