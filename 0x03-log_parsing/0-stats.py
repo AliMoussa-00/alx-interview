455619018d3afd68d2bd22ebcf289928dcfaecca
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-05-02 13:10:12 +0100

    uploading task

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+'''Log parsing'''
+
+import re
+from sys import stdin
+
+i = 0
+total_size = 0
+codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+
+for line in stdin:
+
+    line_pattern = re.compile(
+        r"(\d{1,4})\.(\d{1,4})\.(\d{1,4})\.(\d{1,4}) \- \[(\d{4}\-\d{2}\-\d{2} \d{2}\:\d{2}\:\d{2}\.\d+)\] \"GET \/projects\/260 HTTP\/1\.1\" (\d{3}) (\d+)")
+
+    match = line_pattern.match(line)
+
+    if match:
+        # ip = ".".join(match.groups()[0: 4])
+        status_code = match.groups()[-2]
+        file_size = int(match.groups()[-1])
+        # print(f'{status_code} : {file_size}')
+
+        if status_code in codes.keys():
+            codes[status_code] = codes[status_code] + 1
+        total_size += file_size
+
+    if i > 10:
+        break
+    i += 1
+
+    print(f'File size: {total_size}')
+    for code, value in codes.items():
+        if value != 0:
+            print(f'{code}: {value}')
