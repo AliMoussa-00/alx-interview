f3fb7042736898b0bc2bf0df57ea20ad7fbb19ca
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-05-08 12:24:41 +0100

    check invalide 1 byte

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,8 +11,13 @@ def validUTF8(data: List[int]) -> bool:
     for number in data:
 
         bin_number = f'{number:08b}'
-        len_bin = len(bin_number)
-        if len_bin > 8:
+        len_bits = len(bin_number)
+
+        if len_bits == 8:
+            msb = bin_number[0]
+            return True if msb == 0 else False
+
+        if len_bits > 8:
             # print(len(bin_number))
             # get the two most significant bites of the last 8 bits
             two_msb = bin_number[-8:-6]
